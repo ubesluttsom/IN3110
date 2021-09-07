eb1b4894524e9bafc2bb4a04ba6d13bb3e1d326e
Martin Mihle Nygaard <martin@mihle.no>
2021-09-07 18:48:39 +0200

    Do 2.3; assignment 2 complete
    TODO: I could clean up some more, and convert to a `case` structure.

--- assignment2/track.sh
+++ assignment2/track.sh
@@ -3,25 +3,28 @@
 cmd=$1
 label=$2
 
+# Define LOGFILE when not an environment variable
 if [ "$LOGFILE" == "" ]; then 
   LOGFILE=~/.local/share/timer_logfile
 fi
 
+# Extract last command/action from logfile (and silence "file not found")
 prevcmd=$(tail -n 1 $LOGFILE 2>/dev/null | cut -d' ' -f 1 -)
 
 if [ "$#" -eq "0" ]; then
   echo "usage: track start [label]"
   echo "       track stop"
   echo "       track status"
+  echo "       track log"
   exit 0
 fi
 
 if [ "$cmd" == "start" ]; then
   if [ "$prevcmd" == "START" ] || [ "$prevcmd" == "LABEL" ] ; then
-    echo "Can't start new task concurrent to previous. (End it first.)"
+    echo "Can't start new task concurrent to previous. ('track stop' first.)"
     exit 0
   else
-    echo "START $(date +'%a %d %b %T %Z %Y')" | tee -a $LOGFILE
+    echo "START $(date +'%a %b %e %H:%M:%S %Z %Y')" | tee -a $LOGFILE
     if [ "$label" == "" ]; then
       label='<null>'
     fi
@@ -35,7 +38,7 @@ if [ "$cmd" == "stop" ]; then
     echo "No current task."
     exit 0
   else
-    echo "END $(date +'%a %d %b %T %Z %Y')" | tee -a $LOGFILE
+    echo "END $(date +'%a %b %e %H:%M:%S %Z %Y')" | tee -a $LOGFILE
     exit 1
   fi
 fi
@@ -52,9 +55,22 @@ if [ "$cmd" == "status" ]; then
 fi
 
 if [ "$cmd" == "log" ]; then
-  # Find all LABELS
-  # for each LABEL
-  #   Extract START and END
-  #   Use `date`(?) to calculate difference
-  grep --context=1 "LABEL" $LOGFILE | sed -e '/LABEL/d' -e '/--/d' | cut -d' ' -f 2-
+  grep "LABEL" $LOGFILE | cut -d' ' -f 2 | while read LABEL;
+  do
+    # Extract START and END times for each label:
+    # `grep`: find right lines (notice before/after-context)
+    # `sed`:  delete line with "LABEL" keyword
+    # `cut`:  extract time (to spesific format for `date`)
+    START=$(grep --max-count=1 --before-context=1 "$LABEL" $LOGFILE | \
+            sed -e '/LABEL/d' | \
+            cut -d' ' -f 4,3,7,5,6)
+    END=$(grep --max-count=1 --after-context=1 "$LABEL" $LOGFILE | \
+          sed -e '/LABEL/d' | \
+          cut -d' ' -f 4,3,7,5,6)
+    # Convert START and END times to UTC seconds
+    START=$(date --utc --date "$START" +"%s")
+    END=$(date --utc --date "$END" +"%s")
+    # Print LABEL; use `date` to calculate elapsed seconds & convert format
+    echo "${LABEL}: $(date --utc -d "0 $END sec - $START sec" +'%H:%M:%S')"
+  done
 fi
