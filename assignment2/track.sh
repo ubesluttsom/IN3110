cc96c5cf436bd6814f0ed96cd3d11c72461bc494
Martin Mihle Nygaard <martin@mihle.no>
2021-09-07 13:28:21 +0200

    Do 2.2 `track.sh` (MVP)
    TODO: change to `case` form, add comments, and misc clean-up.

--- assignment2/track.sh
+++ assignment2/track.sh
@@ -1,42 +1,51 @@
-#!/bin/bash -x
+#!/bin/bash
 
 cmd=$1
 label=$2
-LOGFILE=timer_logfile
-prevcmd=$(tail -n 1 $LOGFILE | cut -d' ' -f 1 -)
 
-if [ ! "$#" -eq 1 ]; then
-  echo "USAGE: track start [label]"
+if [ "$LOGFILE" == "" ]; then 
+  LOGFILE=~/.local/share/timer_logfile
+fi
+
+prevcmd=$(tail -n 1 $LOGFILE 2>/dev/null | cut -d' ' -f 1 -)
+
+if [ "$#" -eq "0" ]; then
+  echo "usage: track start [label]"
   echo "       track stop"
   echo "       track status"
   exit 0
 fi
 
-if [ "${cmd}" == "start" ]; then
-  if [ "$prevcmd" == "START" ]; then
+if [ "$cmd" == "start" ]; then
+  if [ "$prevcmd" == "START" ] || [ "$prevcmd" == "LABEL" ] ; then
     echo "Can't start new task concurrent to previous. (End it first.)"
     exit 0
   else
-    echo "START $(date)" >> $LOGFILE
+    echo "START $(date)" | tee -a $LOGFILE
+    if [ "$label" == "" ]; then
+      label='<null>'
+    fi
+    echo "LABEL $label" | tee -a $LOGFILE
     exit 1
   fi
 fi
 
-if [ "${cmd}" == "stop" ]; then
-  if [ "$prevcmd" != "START" ]; then
+if [ "$cmd" == "stop" ]; then
+  if [ "$prevcmd" != "START" ] && [ "$prevcmd" != "LABEL" ] ; then
     echo "No current task."
     exit 0
   else
-    echo "END $(date)" >> $LOGFILE
+    echo "END $(date)" | tee -a $LOGFILE
     exit 1
   fi
 fi
 
-if [ "${cmd}" == "status" ]; then
-  if [ "$prevcmd" == "START" ]; then
-    echo "Currently tracking: $(tail -n 1 $LOGFILE)"
+if [ "$cmd" == "status" ]; then
+  if [ "$prevcmd" == "START" ] || [ "$prevcmd" == "LABEL" ] ; then
+    echo "Currently tracking:"
+    echo "$(tail -n 2 $LOGFILE)"
     exit 1
-  elif [ "$prevcmd" != "START" ]; then
+  else
     echo "No current task."
     exit 1
   fi
