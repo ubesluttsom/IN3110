f3b52355284e93efc114a58adef39f7fd8c2c031
Martin Mihle Nygaard <martin@mihle.no>
2021-10-07 17:49:00 +0200

    Move `grayscale_image()` and `sepia_image()`

--- assignment4/grayscale_image.py
+++
@@ -1,9 +0,0 @@
-from instapy.gray.numpy_color2gray import numpy_color2gray as gray
-from instapy.utils import read_image, save_image
-
-def grayscale_image(input_filename, output_filename=None):
-  image = read_image(input_filename)
-  image = gray(image)
-  if output_filename != None:
-    save_image(output_filename, image)
-  return image
--- assignment4/setup.py
+++ assignment4/setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-import setuptools
+from setuptools import setup
 
-setuptools.setup(
+setup(
     name='instapy',
     version='0.1.0',
     author='Martin Mihle Nygaard',
@@ -13,3 +13,34 @@ setuptools.setup(
     install_requires=['numba', 'numpy', 'opencv-python'],
     scripts=['bin/instapy']
     )
+
+# I have no idea what the assignment text refers to here --- where to put and
+# for what use --- :
+#
+# > Include a function `grayscale_image(input filename, output filename=None)`
+# > which returns a numpy (unsigned) integer 3D array of a gray image of
+# > `input_filename`. If `output_filename` is supplied, the created image
+# > should also be saved to the specified location with the specified name.
+# >
+# > The function `sepia_image(input filename, output filename=None)` should be
+# > implemented in the same way as `grayscale_image()`.
+# 
+# So I just put them below here. I don't use them for anything.
+
+from instapy.sepia.numpy_color2sepia import numpy_color2sepia as sepia
+from instapy.gray.numpy_color2gray import numpy_color2gray as gray
+from instapy.utils import read_image, save_image
+
+def sepia_image(input_filename, output_filename=None):
+  image = read_image(input_filename)
+  image = sepia(image)
+  if output_filename != None:
+    save_image(output_filename, image)
+  return image
+
+def grayscale_image(input_filename, output_filename=None):
+  image = read_image(input_filename)
+  image = gray(image)
+  if output_filename != None:
+    save_image(output_filename, image)
+  return image
